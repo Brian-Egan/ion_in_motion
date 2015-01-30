70d6632504a5d8acbe3834a10c74b9518dba0e74
Brian Egan <BrianEgan@outlook.com>
2015-01-30 16:34:08 -0500

    Small update for ion icons read bug

--- lib/ion_in_motion.rb
+++ lib/ion_in_motion.rb
@@ -9,12 +9,12 @@ Motion::Project::App.setup do |app|
 
   puts "Setting up IonIcons! in #{Dir.pwd}"
   # puts Dir.pwd
-  if Dir.exists?("../vendor/Pods/ionicons")
+  if Dir.exists?("vendor/Pods/ionicons")
     puts "That directory exists!"
-    unless File.exists?("../vendor/Pods/ionicons/ionicons/README.md")
+    unless File.exists?("vendor/Pods/ionicons/ionicons/README.md")
       puts "About to load setup"
       load "setup/setup.rb"
-      File.open("../vendor/Pods/ionicons/ionicons/README.md", 'w') {|f| f.write(SetupIon.readme_text)}
+      File.open("vendor/Pods/ionicons/ionicons/README.md", 'w') {|f| f.write(SetupIon.readme_text)}
     end
   end
 
