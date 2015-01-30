3c72b37086b3907ff78e736dbd3e623cb98eb49b
Brian Egan <BrianEgan@outlook.com>
2015-01-30 16:41:11 -0500

    Fix to README bug
    - Seems to be fully working. Something is up with their CocoaPod though…

--- lib/ion_in_motion.rb
+++ lib/ion_in_motion.rb
@@ -7,13 +7,8 @@ require 'motion-cocoapods'
 
 Motion::Project::App.setup do |app|
 
-  puts "Setting up IonIcons! in #{Dir.pwd}"
-  # puts Dir.pwd
   if Dir.exists?("vendor/Pods/ionicons")
-    puts "That directory exists!"
     unless File.exists?("vendor/Pods/ionicons/ionicons/README.md")
-      puts "About to load setup"
-      load "setup/setup.rb"
       File.open("vendor/Pods/ionicons/ionicons/README.md", 'w') {|f| f.write(SetupIon.readme_text)}
     end
   end
